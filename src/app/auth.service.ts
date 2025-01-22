3dd6be3937ef4b178bd19d753f19354fa64eca04
Your Name <you@example.com>
2025-01-22 14:47:33 +0100

    URL heroku

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -8,7 +8,7 @@ import { User } from './models/user.model';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:3000/auth'; 
+  private apiUrl = 'https://book-app-backend-a3d346b4aef7.herokuapp.com/auth'; 
   userData = signal<User | null>(null);
   alert = signal<string>('');
 
